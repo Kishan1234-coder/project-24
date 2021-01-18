124e2b58232b3040bffdd3d408243f2a7d0b74d7
Kishan1234-coder <76171406+Kishan1234-coder@users.noreply.github.com>
2021-01-18 20:48:57 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,21 @@
+class Dustbin{
+    constructor(x,y,width,height){
+         var options  = {
+          isStatic : true,
+          //restitution : 0.3,
+          friction : 2,
+          density : 1.2
+
+      }
+      this.body = Bodies.rectangle(x,y,width,height);
+      this.width = width;
+      this.height = height;
+      World.add(world , this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("white");
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
